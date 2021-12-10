208d19c6fe2df6b8046951f9524f913975c3dd4b
Facundo Jannello <facundojannellobaldi@gmail.com>
2021-12-09 21:08:09 -0300

    Creado el desafío de la clase 6

---
+++ clase-6/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+     <h1>Estos son algunos productos: </h1>
+     <div class="d-flex mt-5 justify-content-center">
+        <ProductCard
+          v-for="card in cards"
+          :key="card"
+          :title="card.title"
+          :price="card.price"
+          :image="card.image"
+          :description="card.description"
+        ></ProductCard>
+     </div>
+  </div>
+</template>
+
+<script>
+import ProductCard from './components/ProductCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductCard
+  },
+  data () {
+    return {
+       cards: [
+         {
+           title: "Smart TV",
+           description: "Una tele familiar",
+           price: 40000,
+           image: {
+             src: "https://http2.mlstatic.com/D_NQ_NP_682817-MLA47690303593_092021-O.webp",
+             alt: "Smart TV foto"
+           } 
+         },
+         {
+           title: "Moto G30",
+           description: "Un alto celu",
+           price: 25000,
+           image: {
+             src: "https://www.naldo.com.ar/medias/405422.jpg-515Wx515H?context=bWFzdGVyfHJvb3R8MTYxMzN8aW1hZ2UvanBlZ3xoNzAvaGEzLzk1ODgwNzEwMzkwMDYuanBnfGM2NWYwMzRhMzI0NzU0NWEwYjEwNjhjNDEwM2FlMjFmNGEzOTkzNWVhNzc0NDZhNmI0ZDNkMmJhNjZiNGI4YTY",
+             alt: "Moto630 Foto"
+           }
+         }  
+       ] 
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ clase-6/src/components/ProductCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" style="max-height: 200px;" :src="image.src" :alt="image.alt">
+    <div class="card-body">
+      <h5 class="card-title">{{ title }}</h5>
+      <h6 class="card-title">$ {{ price }}</h6>
+      <p class="card-text">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProductCard',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    description: {
+      type: String,
+      required: false
+    },
+    image: {
+      type: Object,
+      required: true
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
